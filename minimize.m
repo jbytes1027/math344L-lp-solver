f4730fe63867425da4e3999bc386efedfff419c0
James Pretorius <jamesmpretorius@gmail.com>
2023-11-22 18:58:52 -0500

    working

--- minimize.m
+++ minimize.m
@@ -1,6 +1,7 @@
 % by James Pretorius
 % first row of inputs is objective function
 function [max] = minimize(z, choice, slack, artificial, b)
+    format rat % debug
     Ab = [z choice slack artificial b] % debug
 
     % used to track where things are
@@ -11,7 +12,7 @@ function [max] = minimize(z, choice, slack, artificial, b)
     numArtificial = size(artificial, 2)
     startColArtificial = startColSlack + numSlack;
 
-    % move M out of artificial columns
+    % MOVE M OUT OF ARTIFICIAL COLUMNS
     % loop through the artificial columns
     for currIndexArtificialCol = startColArtificial:startColArtificial + (numArtificial - 1)
         % find the row for artifical value of the current col
@@ -27,72 +28,77 @@ function [max] = minimize(z, choice, slack, artificial, b)
     end
 
     afterMoving = Ab % debug
-    return
 
-    for nthNonbasic = 1:size(nonbasic, 2)
-        if isMaximized(nonbasic)
-            break
+    % GET THE BASIC COLUMNS
+    basicCols = [];
+    for col = 2:(size(Ab, 2)-1) % from 2 to end-1
+        % if more than one non-zero in column
+        if nnz(Ab(:, col)) > 1
+            basicCols = [basicCols col];
+        end
+    end
+
+    basicCols
+
+    while true
+        % FIND THE SMALLEST NEGATIVE COEFFICIENT IN THE OBJECTIVE FUNCTION (IN THE BASIC COLUMNS)
+        indexSmlSolCoeff = -1;
+        currSmallest = 0;
+        for col = basicCols
+            if Ab(1, col) < currSmallest
+                indexSmlSolCoeff = col
+                currSmallest = Ab(1, col)
+            end
         end
 
-        sourcePivotRow = findPivotRow(nthNonbasic, nonbasic, b)
-        % make every other pivot row have 0 in the nthBasic column
+        if indexSmlSolCoeff == -1
+            break % nothing left todo
+        end
+
+
+        % FIND THE CONSTRAINT ROW WITH THE SMALLEST POSITIVE RATIO
+        minRatio = intmax;
+        pivotRow = 0;
+        % loop through all constraint rows (1st row is the objective function)
+        for currRow = 2:size(Ab, 1)
+            currRow
+            indexSmlSolCoeff
+            Ab(currRow, indexSmlSolCoeff)
+            rowRatio = Ab(currRow, end)/Ab(currRow, indexSmlSolCoeff)
+            % if the ratio is the lowest and is positive
+            if rowRatio <= minRatio && rowRatio > 0
+                pivotRow = currRow;
+                minRatio = rowRatio;
+            end
+        end
+
+        if pivotRow == 0
+            error("could not find a pivot row")
+        end
+
+
+        % MAKE EVERY OTHER ROW IN PIVOT COL 0
         for destPivotRow = 1:size(z, 1)
-            if destPivotRow == sourcePivotRow
+            if destPivotRow == pivotRow
                 continue
             end
 
-            multiplier = nonbasic(destPivotRow, nthNonbasic)/nonbasic(sourcePivotRow, nthNonbasic)
-            [z, nonbasic, slack, artificial, b] = pivot(multiplier, sourcePivotRow, destPivotRow, z, nonbasic, slack, artificial, b);
-
-            curr = [z nonbasic slack artificial b] % debug
+            Ab = pivot(destPivotRow, indexSmlSolCoeff, pivotRow, Ab);
         end
+
+        afterPivot = Ab % debug
     end
 
-
-    if ~isMaximized(nonbasic)
-        error("failed to minimize")
-    end
-
-    max = b(1)
+    max = Ab(1, end)
 end
 
 % Makes (destRow, col)=zero by a pivot from (sourceRow, col)
 function AbOut = pivot(destRow, col, sourceRow, Ab)
-    multiplier = Ab(destRow, col)/Ab(sourceRow, col)
+    multiplier = Ab(destRow, col)/Ab(sourceRow, col);
 
     Ab(destRow, :) -= multiplier*Ab(sourceRow, :);
 
     AbOut = round6(Ab);
-end
-
-function is = isMaximized(nonbasic)
-    % is all elements in first row (aka non basic vars of objective function) greater than zero?
-    is = true;
-    for col = 1:size(nonbasic, 2)
-        if nonbasic(1, col) < 0
-            is = false;
-            return
-        end
-    end
-end
-
-% find the constraint row with the smallest ratio
-function pivotRow = findPivotRow(nthNonbasic, nonbasic, b)
-    % loop through all constraint rows (1st row is the objective function)
-    minRatio = intmax;
-    pivotRow = 0;
-    for currRow = 2:size(nonbasic, 1)
-        rowRatio = round6(b(currRow)/nonbasic(currRow, nthNonbasic));
-        % if the ratio is the lowest and is positive
-        if rowRatio <= minRatio && rowRatio > 0
-            pivotRow = currRow;
-            minRatio = rowRatio;
-        end
-    end
-
-    if pivotRow == 0
-        error("could not find a pivot row")
-    end
 end
 
 % round to six decimals
